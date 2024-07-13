c6590483d0279a292d71fa3694dec3163f537210
GrkDev <eligombis.yt3@gmail.com>
2024-07-13 15:23:34 -0500

    Added Basic Functionality to Randomizer Command

--- cogs/randomizer.py
+++ cogs/randomizer.py
@@ -2,6 +2,7 @@ import discord
 import random
 from discord.ext import commands
 from discord import app_commands
+import time
 
 class Randomizer(commands.Cog):
     def __init__(self, client):
@@ -12,8 +13,24 @@ class Randomizer(commands.Cog):
         print("Randomizer Cog is online")
     
     @app_commands.command(name = "randomizer", description = "Pick a random option! Lots of setting available!")
-    async def randomizer(self, interaction:discord.Interaction, options: str):
-        await interaction.response.send_message("This command isn't ready yet, come back later!")
+    async def randomizer(self, interaction: discord.Interaction, options: str, wait: int):
+        loadEmoji = "<a:loading:1261772710539952149>"
+        optionsList = []
+        options = options.replace(", ", ",").replace(" ,", ",") + ","
+
+        for i in options:
+            if i == ",":
+                optionsList.append(options[:options.index(",")])
+                options = options[options.index(",")+1:]
+        msg = ""
+        for i in optionsList:
+            msg = msg + i + "\n"
+        msg = msg + "\nThinking " + loadEmoji
+        await interaction.response.send_message(msg)
+
+        time.sleep(wait)
+
+        await interaction.edit_original_response(content = f"The Winner is... {optionsList[random.randint(0, (len(optionsList) - 1))]}")
 
 async def setup(client):
     await client.add_cog(Randomizer(client))
