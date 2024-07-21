1755cd22a4ce4ae43e370179b7a1dfa1d5f452a9
GrkDev <eligombis.yt3@gmail.com>
2024-07-21 18:34:57 -0500

    Added Preset List Command

--- cogs/preset.py
+++ cogs/preset.py
@@ -66,6 +66,8 @@ class Preset(commands.Cog):
 
             cursor.execute("INSERT INTO presets(name, description, options, weights, waitTime, userID) VALUES (?, ?, ?, ?, ?, ?)", (self.name, self.description, self.options, self.weights, self.waitTime, interaction.user.id))
             db.commit()
+            cursor.close()
+            db.close()
 
             await interaction.response.send_message(embed = discord.Embed(title = f"Your Preset __{self.name}__ was created", color = discord.Colour.green()))
         
@@ -73,6 +75,57 @@ class Preset(commands.Cog):
     @preset.command(name = "create", description = "Create a randomizer preset and save it to use later")
     async def create(self, interaction: discord.Interaction):
         await interaction.response.send_modal(self.CreateModal())
+
+    @preset.command(name = "list", description = "Lists the randomizer presets you have created")
+    async def list(self, interaction: discord.Interaction):
+        embed =  discord.Embed(title = f"{interaction.user.name}'s Randomizer Presets", color = discord.Colour.blue())
+        
+        db = sqlite3.connect("main.sqlite")
+        cursor = db.cursor()
+
+        cursor.execute(f"SELECT * FROM presets WHERE userID = {interaction.user.id}")
+        resultsUnformatted = cursor.fetchall()
+        
+        results = []
+        for i in resultsUnformatted:
+            i = list(i)
+            optionsList = []
+            i[2] = i[2].replace(", ", ",").replace(" ,", ",") + ","
+            for n in i[2]:
+                if n == ",":
+                    optionsList.append(i[2][:i[2].index(",")])
+                    i[2] = i[2][i[2].index(",")+1:]
+
+            i[2] = optionsList
+
+            weightsList = []
+            i[3] = i[3].replace(", ", ",").replace(" ,", ",") + ","
+            for n in i[3]:
+                if n == ",":
+                    try:
+                        weightsList.append(int(i[3][:i[3].index(",")]))
+                        i[3] = i[3][i[3].index(",")+1:]
+                    except:
+                        weightsList.append(1)
+                        i[3] = i[3][i[3].index(",")+1:]
+
+            for n in range(len(optionsList)):
+                try:
+                    emptyVar = weightsList[n]
+                except:
+                    weightsList.append(1)
+
+            i[3] = weightsList
+
+            results.append(i)
+            
+        for i in results:
+            fieldValue = f"Description:\n{i[1]}\n\nWait Time: {i[4]}\n\n"
+            for n in range(len(i[2])):
+                fieldValue = fieldValue + f"Option: {i[2][n]} - Weight: {i[3][n]}\n"
+            embed.add_field(name = i[0], value = fieldValue, inline = False)
+
+        await interaction.response.send_message(embed = embed, ephemeral = True)
     
 async def setup(client):
     await client.add_cog(Preset(client))
